56ed0cc1088de74003743c178827a4cc5e172dc7
没有蜡 <zhengyq93@163.com>
2020-06-30 08:47:15 +0800

    first commit

---
+++ FirstXamarin/FirstXamarin/Model/NoteObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstXamarin.Model
+{
+    public class NoteObject
+    {
+        public string FileName { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ FirstXamarin/FirstXamarin/NoteListPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using FirstXamarin.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteListPage : ContentPage
+    {
+        public NoteListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            var notes = new List<NoteObject>();
+            var files = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "*.notes.txt");
+            foreach(var filename in files)
+            {
+                notes.Add(new NoteObject()
+                {
+                    FileName = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                }) ;
+            }
+            listView.ItemsSource = notes.OrderBy(x => x.Date).ToList();
+        }
+
+        async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage
+            {
+                BindingContext = new NoteObject()
+            });
+        }
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new MainPage
+                {
+                    BindingContext = e.SelectedItem as NoteObject
+                });
+            }
+        }
+    }
+}
